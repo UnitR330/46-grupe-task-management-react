d5f70192e4f6f389125f79d15157d5d7b388426d
UnitR330 <145355731+UnitR330@users.noreply.github.com>
2023-11-14 11:40:25 +0200

    Update

---
+++ src/components/kanban/Task.card.jsx
@@ -0,0 +1,12 @@
+import style from './Kanban.module.css';
+import { Tag } from './Tag';
+
+export function taskCard({title, description, deadline, tags}) {
+    console.log(tags);
+
+
+
+    // console.log(props.tag);
+    // const tags  props.tags;
+    // const {tags} = props;
+}
